694ca6d919961794d9d6234b0129c66e7160906b
herukrayz <ad20h4y@yahoo.com>
2021-08-19 19:00:23 +0700

    Create create.sh

---
+++ create.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# $1 = VM ID
+# $2 = Image name
+# $3 = Target Storage
+
+qm create $1 --net0 virtio,bridge=vmbr0
+qm importdisk $1 $2 $3
+qm set $1 --scsihw virtio-scsi-pci --scsi0 $3:vm-$1-disk-0
+qm set $1 --ide0 $3:cloudinit
+qm set $1 --boot c --bootdisk scsi0
+qm set $1 --serial0 socket --vga serial0
+qm template $1
